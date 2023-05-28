7ad4aaaf9dca6e92bb52d0ded79cee75fb2b1306
ShimiyonS <shimjnss@gmail.com>
2023-05-28 21:44:20 +0530

    Final

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import orderRoutes from "./routes/orderRoute.js";
 import { errorHandler } from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(express.json());
 
