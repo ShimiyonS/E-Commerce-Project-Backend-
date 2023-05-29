566836f1e90a9e35f064d85e08d36dde110041bb
ShimiyonS <shimjnss@gmail.com>
2023-05-29 08:55:08 +0530

    LAST_PUSH

--- BackEnd/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_KEY, {
-    expiresIn: "15d",
-  });
-};
-
-export default generateToken;
